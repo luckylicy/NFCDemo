daa59c7203c882c9722cdb710732152b8f002b42
lichengyang <licy3051@qq.com>
2020-10-09 11:28:21 +0800

    commit 3

--- app/src/main/java/licy/app/nfcdemo/NfcActivity.java
+++ app/src/main/java/licy/app/nfcdemo/NfcActivity.java
@@ -7,12 +7,12 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import licy.app.nfcdemo.databinding.ActivityMainBinding;
@@ -42,29 +42,45 @@ public class NfcActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tv_content);
         textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 
+
+        mActivityMainBinding.rgIndex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                mActivityMainBinding.etText.setText("");
+            }
+        });
+
         //读卡
         mActivityMainBinding.btnRead.setOnClickListener(v -> {
             if (M1CardUtils.hasCardType(mTag, this, "MifareClassic")) {
                 try {
                     StringBuilder stringBuilder = new StringBuilder();
                     M1CardUtils.readCard(mTag);
-                    String m1Content = M1CardUtils.readCard(mTag, 1);
-                    textView.setText(m1Content);
-                    Toast.makeText(this, "读取成功" + m1Content, Toast.LENGTH_SHORT).show();
+                    String tagFirst = M1CardUtils.readCard(mTag, 1);
+                    String tagSecond = M1CardUtils.readCard(mTag, 2);
+                    String tags = tagFirst.trim() + tagSecond.trim();
+                    textView.setText(tags);
+                    Toast.makeText(this, "读取成功" + tags, Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(this, "读取失败" , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "读取失败", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        //CPU
+        // 写卡
         mActivityMainBinding.btnWrite.setOnClickListener(v -> {
             try {
+                int blockIndex = 1;
+                if (mActivityMainBinding.rbIndex1.isChecked()) {
+                    blockIndex = 1;
+                } else if (mActivityMainBinding.rbIndex2.isChecked()) {
+                    blockIndex = 2;
+                }
                 String trim = mActivityMainBinding.etText.getText().toString().trim();
                 trim = StringUtil.fillRight(trim, " ", 16);
                 byte[] bytes = trim.getBytes(StandardCharsets.UTF_8);
-                if (M1CardUtils.writeBlock(mTag, 1, bytes)) {
+                if (M1CardUtils.writeBlock(mTag, blockIndex, bytes)) {
                     Log.e("M1CardUtils", "写入成功");
                     Toast.makeText(this, "写入成功", Toast.LENGTH_SHORT).show();
                 } else {
