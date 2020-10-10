14a1c1ea59971c3f066ebbf8fad83a7f910301d7
lichengyang <licy3051@qq.com>
2020-10-10 11:18:48 +0800

    commit 4

--- app/src/main/java/licy/app/nfcdemo/NfcActivity.java
+++ app/src/main/java/licy/app/nfcdemo/NfcActivity.java
@@ -7,7 +7,6 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,12 +42,12 @@ public class NfcActivity extends AppCompatActivity {
         textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 
 
-        mActivityMainBinding.rgIndex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                mActivityMainBinding.etText.setText("");
-            }
-        });
+//        mActivityMainBinding.rgIndex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                mActivityMainBinding.etText.setText("");
+//            }
+//        });
 
         //读卡
         mActivityMainBinding.btnRead.setOnClickListener(v -> {
@@ -71,21 +70,33 @@ public class NfcActivity extends AppCompatActivity {
         // 写卡
         mActivityMainBinding.btnWrite.setOnClickListener(v -> {
             try {
-                int blockIndex = 1;
-                if (mActivityMainBinding.rbIndex1.isChecked()) {
-                    blockIndex = 1;
-                } else if (mActivityMainBinding.rbIndex2.isChecked()) {
-                    blockIndex = 2;
-                }
                 String trim = mActivityMainBinding.etText.getText().toString().trim();
-                trim = StringUtil.fillRight(trim, " ", 16);
-                byte[] bytes = trim.getBytes(StandardCharsets.UTF_8);
-                if (M1CardUtils.writeBlock(mTag, blockIndex, bytes)) {
-                    Log.e("M1CardUtils", "写入成功");
-                    Toast.makeText(this, "写入成功", Toast.LENGTH_SHORT).show();
+                if (trim.length() >= 16) {
+                    String first = trim.substring(0, 16);
+                    byte[] bytesFirst = first.getBytes(StandardCharsets.UTF_8);
+                    boolean boolFirst = M1CardUtils.writeBlock(mTag, 1, bytesFirst);
+
+                    String second = trim.substring(16);
+                    second = StringUtil.fillRight(second, " ", 16);
+                    byte[] bytesSecond = second.getBytes(StandardCharsets.UTF_8);
+                    boolean boolSecond = M1CardUtils.writeBlock(mTag, 2, bytesSecond);
+                    if (boolFirst && boolSecond) {
+                        Log.e("M1CardUtils", "写入成功");
+                        Toast.makeText(this, "写入成功", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.e("M1CardUtils", "写入失败");
+                        Toast.makeText(this, "写入失败", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Log.e("M1CardUtils", "写入失败");
-                    Toast.makeText(this, "写入失败", Toast.LENGTH_SHORT).show();
+                    trim = StringUtil.fillRight(trim, " ", 16);
+                    byte[] bytes = trim.getBytes(StandardCharsets.UTF_8);
+                    if (M1CardUtils.writeBlock(mTag, 1, bytes)) {
+                        Log.e("M1CardUtils", "写入成功");
+                        Toast.makeText(this, "写入成功", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.e("M1CardUtils", "写入失败");
+                        Toast.makeText(this, "写入失败", Toast.LENGTH_SHORT).show();
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
